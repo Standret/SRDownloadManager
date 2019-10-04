3fc6bd6bd3e718c94f96ff923adc2505f5673f47
Standret <pstandret@gmail.com>
2019-10-04 16:06:37 +0300

    added MD5 hashing

--- SRDownloadManager/SRDownloadManager.m
+++ SRDownloadManager/SRDownloadManager.m
@@ -11,7 +11,7 @@
 #define SRDownloadDirectory self.cacheFilesDirectory ?: [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).lastObject \
 stringByAppendingPathComponent:NSStringFromClass([self class])]
 
-#define SRFileName(URL) [URL lastPathComponent] // use URL's last path component as the file's name
+#define SRFileName(URL) [NSString stringWithFormat:@"%@.%@", [URL path].MD5, [URL pathExtension]] // use URL's last path component as the file's name
 
 #define SRFilePath(URL) [SRDownloadDirectory stringByAppendingPathComponent:SRFileName(URL)]
 
@@ -31,7 +31,39 @@ stringByAppendingPathComponent:NSStringFromClass([self class])]
 
 @end
 
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSString(MD5)
+
+#pragma mark - MD5
+
+- (NSString*)MD5
+{
+    // Create pointer to the string as UTF8
+    const char *ptr = [self UTF8String];
+    
+    // Create byte array of unsigned chars
+    unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
+    
+    // Create 16 byte MD5 hash value, store in buffer
+    CC_MD5(ptr, strlen(ptr), md5Buffer);
+    
+    // Convert MD5 value in the buffer to NSString of hex values
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x",md5Buffer[i]];
+    
+    return output;
+}
+
+@end
+
 @implementation SRDownloadManager
+
+//- (NSString *)SRFileName:(NSURL *)url {
+//    return [url path].MD5;
+//}
+
 
 #pragma mark - Lazy Load
 
